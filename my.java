95a61746ec50e49af001a53c926dab6d17671000
chenpeng <du5248jiaogufu2@163.com>
2017-07-29 10:01:41 +0800

    Update my.java

--- my.java
+++ my.java
@@ -2,3 +2,37 @@
 Acting as if nothing borne in mind is the best revenge . It's all for myself to live better.*/
 /*2、失败时有人伸出一只手来为你擦泪，会好过成功时无数人伸手为你鼓掌。
 A single hand that wipes tears during failures is much better than countless hands that come together to clap on success*/
+
+好好学习，天天向上！
+
+累吗
+累就对了
+舒服是留给死人的
+
+You are your own hero
+
+I don't know the young
+and frivolous,I only konw that the winner takes all
+
+自己选择的路，在艰难，跪着也要走下去
+
+We can't create the world but we can change it.
+
+梦想，是一步一个脚印走出来的。
+
+我若不勇敢，谁替我坚强。
+
+人若有志，万事可为。
+
+我希望早上叫我起床的不是闹钟，而是梦想！
+
+人生如路，须在荒凉中走出繁华的风景。
+
+低头不是认输，是要看清自己的路。
+仰头不是骄傲，是要看见自己的天空。
+
+没有尽过自己最大的努力，就没有资格说不行。
+
+只要有一种无穷的自信充满了心灵，再凭着坚强的意志和独立不羁的才智，总有一天会成功。 ----莫泊桑
+
+好时光都应该被宝贝，因为有限。
