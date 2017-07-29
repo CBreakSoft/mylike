b07fab03a6b559d19ca41ba5472832f11964a873
chenpeng <du5248jiaogufu2@163.com>
2017-07-29 10:02:24 +0800

    Update my.java

--- my.java
+++ my.java
@@ -1,7 +1,9 @@
-/*1、若无其事，原来是最好的报复。生活得更好，是为了自己。
-Acting as if nothing borne in mind is the best revenge . It's all for myself to live better.*/
-/*2、失败时有人伸出一只手来为你擦泪，会好过成功时无数人伸手为你鼓掌。
-A single hand that wipes tears during failures is much better than countless hands that come together to clap on success*/
+
+若无其事，原来是最好的报复。生活得更好，是为了自己。
+Acting as if nothing borne in mind is the best revenge . It's all for myself to live better.
+  
+失败时有人伸出一只手来为你擦泪，会好过成功时无数人伸手为你鼓掌。
+A single hand that wipes tears during failures is much better than countless hands that come together to clap on success
 
 好好学习，天天向上！
 
