69bf0023e54615d900a7e91fe886a291de68ad1f
chenpeng <du5248jiaogufu2@163.com>
2017-08-10 21:55:57 +0800

    Update my.java

--- my.java
+++ my.java
@@ -38,3 +38,19 @@ We can't create the world but we can change it.
 只要有一种无穷的自信充满了心灵，再凭着坚强的意志和独立不羁的才智，总有一天会成功。 ----莫泊桑
 
 好时光都应该被宝贝，因为有限。
+
+
+"If you are not willing to risk the unusual,you will have to settle for the ordinary."                                                           
+-Jim Rohn,Author
+"如果你不愿意去冒险做不一样的事，你就得和一般般妥协 "
+-吉姆．维恩(励志作家)
+
+"For success, attitude is equally as important as ability."
+-Walter Scott,Novelist
+“为取得成功，态度和能力一样重要。”
+-华特.司各特（小说家）
+
+"There exists in the world a single path along which no one can go except you:whither does it lead?Do not ask go along it. "
+-Friedrich Nietzsche,philosopher
+"这世上存在一条除了你没人能走得单行道：它会通向哪里？不要问，顺着它走就是了。"
+-弗里德里希.尼采（哲学家）
